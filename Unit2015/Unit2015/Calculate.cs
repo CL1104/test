dde7858b48b14238f13a1bd9505bc6dc13cc90bf
cl <fanlin@163.com>
2019-03-27 17:11:39 +0800

    2015单元测试

---
+++ Unit2015/Unit2015/Calculate.cs
@@ -0,0 +1,26 @@
+﻿
+
+namespace Unit2015
+{
+   public class Calculate
+    {
+        public static int GetSum(int value)
+        {
+            int sum = 0;
+            for (int i = 1; i <= value; i++)
+            {
+                sum += i;
+            }
+            return sum;
+        }
+        public static int GetSum1(int value)
+        {
+            int sum = 0;
+            for (int i = 1; i <= value; i++)
+            {
+                sum += i;
+            }
+            return sum;
+        }
+    }
+}
---
+++ Unit2015/Unit2015Tests/CalculateTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Unit2015;
+
+
+namespace Unit2015.Tests
+{
+    [TestClass()]
+    public class CalculateTests
+    {
+        [TestMethod()]
+        public void GetSumTest()
+        {
+            Assert.IsTrue(Calculate.GetSum(10) == 55);
+        }
+
+        [TestMethod()]
+        public void GetSum1Test()
+        {
+            Assert.IsTrue(Calculate.GetSum(10) == 56);
+        }
+    }
+}
